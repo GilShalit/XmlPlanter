ced571605392c364716778682d9b67f6b77d23e7
Gil Shalit <gil.shalit@gmail.com>
2021-02-16 18:00:42 +0200

    loading files

---
+++ TeiEditor/wwwroot/utils.js
@@ -0,0 +1,13 @@
+﻿window.WindowInerrHeight = function () {
+    return window.innerHeight;
+}
+window.WindowInnerWidth = function () {
+    return window.innerWidth;
+}
+window.resizeEditors = function (id) {
+    let wh = window.innerHeight;
+    var sourceE = document.getElementById(id);
+    var height = `${window.innerHeight - 150}px`;
+    sourceE.style.height = height;
+    return height
+}
