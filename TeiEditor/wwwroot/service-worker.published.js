8e6a1f6665bd1bf898a85e6f5170058b6f654bf2
Gil Shalit <gil.shalit@gmail.com>
2021-03-29 13:18:17 +0300

    v.0.8.1.1

--- TeiEditor/wwwroot/service-worker.published.js
+++ TeiEditor/wwwroot/service-worker.published.js
@@ -1,6 +1,8 @@
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
+const CACHE_VERSION = '0.8.1.1';
+
 self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
